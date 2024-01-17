0be0d24576b489ff0b42d2fa0d020764f18b14ce
rosca <rosca.adelin@gmail.com>
2024-01-17 09:49:30 +0100

    css setup & header

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import { NavLink } from 'react-router-dom';
+import '../App.css';
+
+const Navbar = () => {
+	return (
+		<nav className='header flex'>
+			<NavLink to='/' className='logo fs-600 text-dark'>
+				myCity
+			</NavLink>
+			<ul className='navbar flex fs-400 '>
+				<li>
+					<NavLink to='/' className='nav-link text-dark'>
+						Home
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/module1' className='nav-link text-dark '>
+						Module 1
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/module2' className='nav-link text-dark '>
+						Module 2
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/contact' className='nav-link text-dark '>
+						Contact
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/login' className='nav-link text-dark '>
+						Login
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
